885514de18f8d883cc48bdc5d354825903a0fe0e
JackBecknell <52moosefit@gmail.com>
2022-04-22 17:02:43 -0400

    Created a variable and declared its value as 'int' + set it equal to a Linq Query utilizing .Count to retrieve nums of users

--- DatabaseFirstLINQ/Program.cs
+++ DatabaseFirstLINQ/Program.cs
@@ -6,9 +6,8 @@ namespace DatabaseFirstLINQ
     {
         static void Main(string[] args)
         {
-            //Problems problems = new Problems();
-            //problems.RunLINQQueries();
-            Console.WriteLine("Heyyo!");
+            Problems problems = new Problems();
+            problems.RunLINQQueries();
         }
     }
 }
