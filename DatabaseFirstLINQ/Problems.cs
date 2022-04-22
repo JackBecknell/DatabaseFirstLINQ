64cf8aff7818614978749b6fa3eacde1388de211
JackBecknell <52moosefit@gmail.com>
2022-04-22 17:06:22 -0400

    Set anonymous var equal to a Linq query where object price is > 150

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -58,17 +58,17 @@ namespace DatabaseFirstLINQ
 
         }
 
-        //private void ProblemThree()
-        //{
-        //    // Write a LINQ query that gets each product where the products price is greater than $150.
-        //    // Then print the name and price of each product from the above query to the console.
-        //    var expensiveProducts = _context.Products.Where(p => p.Price > 150);
+        private void ProblemThree()
+        {
+            // Write a LINQ query that gets each product where the products price is greater than $150.
+            // Then print the name and price of each product from the above query to the console.
+            var expensiveProducts = _context.Products.Where(p => p.Price > 150);
 
-        //    foreach (Product product in expensiveProducts)
-        //    {
-        //        Console.WriteLine(product.Name + " $" + product.Price);
-        //    }
-        //}
+            foreach (Product product in expensiveProducts)
+            {
+                Console.WriteLine(product.Name + " $" + product.Price);
+            }
+        }
 
         //private void ProblemFour()
         //{
